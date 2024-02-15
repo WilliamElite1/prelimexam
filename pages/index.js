d51cd25c64cccaedeb9f2ad4f3e420e3c61bd691
WilliamElite1 <143056290+WilliamElite1@users.noreply.github.com>
2024-02-15 12:08:49 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <Layout home>
       <Head>
-        <title>{siteTitle}</title>
+        <title>No Talent Required</title>
       </Head>
       <section className={utilStyles.headingMd} style={{ textAlign: 'center', fontSize: '2rem' }}>
         <p> (This is a sample website - you’ll be building a site like this on <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)</p>
